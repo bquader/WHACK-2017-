dc91ae10f1ade4388c5b12ee1b73a3b05ca97d3f
Anton Paquin <antonpaquin@gmail.com>
2017-02-11 19:44:01 -0500

    made database work

---
+++ quotes/db.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+conn = sqlite3.connect('quotes.db')
+c = conn.cursor()
+
+def store(tweetContent, scores):
+    c.execute('''INSERT INTO tweets(CONTENT) VALUES (?)''', (tweetContent,))
+    tweetid = c.lastrowid
+    for word, score in scores:
+        c.execute('''INSERT INTO scores(TWEETID, WORD, SCORE) VALUES (?,?,?)''',
+            (tweetid, word, score))
+
+    conn.commit()
+
+def scoreSearch(word, size=1):
+    scores = c.execute('''SELECT * FROM scores WHERE WORD = ?''', (word,)).fetchall()
+
+    if len(scores) == 0:
+        return 'No quotes found!'
+
+    sortedScores = sorted(scores, key=lambda x: x[3])
+
+    quotes = []
+    for i in range(size):
+        quoteIndex = sortedScores[i][1]
+        quote = c.execute('''SELECT * FROM tweets WHERE ID = ?''', (quoteIndex,)).fetchone()
+        quotes.append(quote[1])
+    return quotes
+
+
+
+def BUILD():
+    c.execute('''CREATE TABLE tweets (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        CONTENT TEXT
+    )''')
+
+    c.execute('''CREATE TABLE scores (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        TWEETID INT,
+        WORD TEXT,
+        SCORE INT
+    )''')
+
+    conn.commit()
--- quotes/interface.py
+++ quotes/interface.py
@@ -1,30 +1,35 @@
 import twitter
 import json
 
+import db
+
 def search(qstring, tweets):
     return list(filter(lambda x: x['text'].find(qstring) != -1, tweets))
 
-def wordIsGood(word):
-    return True
+def doRunTweet(tweet):
+    sTweet = sortTweet(tweet['text'])
+    tweetScores = processSortedTweet(sTweet)
+    db.store(tweet['text'], tweetScores)
 
-with open('ea-thesaurus.json') as thesaurus_f:
-    thesaurus = json.loads(thesaurus_f.read(), 'lxml')
 def getSynonyms(word):
     if word.upper() in thesaurus:
         return thesaurus[word.upper()]
     else:
-        return [{ word.upper(): '20' }]
+        return [{ word.upper(): '10' }]
+with open('ea-thesaurus.json') as thesaurus_f:
+    thesaurus = json.loads(thesaurus_f.read(), 'lxml')
 
-with open('stopwords.json') as stopwords_f:
-    stopwords = json.loads(stopwords_f.read(), 'lxml')
 def sortTweet(text):
     processedTweet = text.upper()
-    processedTweet = processedTweet.split(' ')
+    processedTweet = processedTweet.replace('.', ' ').replace('\n', ' ')
+    processedTweet = ''.join(filter(lambda x: str.isalpha(x) or x == ' ', processedTweet))
+    print(processedTweet)
+    processedTweet = processedTweet.split()
     processedTweet = [word for word in processedTweet if not (word in stopwords)]
     wordScores = {}
     for word in processedTweet:
         synonyms = getSynonyms(word)
-        synonyms.append({ word : '30' })
+        synonyms.append({ word : '80' })
         for s in synonyms:
             synonym = list(s.keys())[0]
             score = int(s[synonym])
@@ -33,3 +38,11 @@ def sortTweet(text):
             else:
                 wordScores[synonym] = score
     return wordScores
+with open('stopwords.json') as stopwords_f:
+    stopwords = json.loads(stopwords_f.read(), 'lxml')
+
+def processSortedTweet(scores, threshold=10):
+    s = list(scores.items())
+    s = [item for item in s if item[1] >= threshold]
+    s = sorted(s, key=lambda x: x[1], reverse=True)
+    return s
